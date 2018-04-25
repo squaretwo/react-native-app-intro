d14c63345705a0a626504aeee7ca3b5774572082
Raymond Cook <lunarraid@gmail.com>
2018-04-25 12:54:02 -0400

    Remove swiper dependency, use prop-types package

--- AppIntro.js
+++ AppIntro.js
@@ -1,5 +1,6 @@
 import assign from 'assign-deep';
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   StatusBar,
   StyleSheet,
@@ -11,7 +12,7 @@ import {
   Image,
   Platform,
 } from 'react-native';
-import Swiper from 'react-native-swiper';
+import Swiper from './components/Swiper';
 import DoneButton from './components/DoneButton';
 import SkipButton from './components/SkipButton';
 import RenderDots from './components/Dots';
